53cbe6bf1bfca29b58d2715da431620b9a2615cf
ETHEREAL28 <edwinwinanda28@gmail.com>
2025-12-15 22:03:31 +0700

    update poster dan web project

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -7,8 +7,18 @@ import Poster3 from "../assets/posters/poster3.jpg";
 import Poster4 from "../assets/posters/poster4.jpg";
 import Poster5 from "../assets/posters/poster5.jpg";
 import Poster6 from "../assets/posters/poster6.jpg";
+import Poster7 from "../assets/posters/uiuixcoffeshop.png";
+import Poster8 from "../assets/posters/postermaroon2024.png";
+import Poster9 from "../assets/posters/mlbbmaroon2024.png";
+import Poster10 from "../assets/posters/bannerrdk.png";
+import Poster11 from "../assets/posters/stikerrdk.png";
+import Poster12 from "../assets/posters/idulfitri.png";
+import Poster13 from "../assets/posters/isramiraj.png";
+
 
 import Portoweb from "../assets/images/edwinportoweb.png";
+import Terserahmart from "../assets/images/terserahmart.png";
+import Coffeshop from "../assets/images/coffeshop.png";
 
 const Projects = () => {
   const [visible, setVisible] = useState(false);
@@ -73,12 +83,12 @@ const Projects = () => {
               style={{ transitionDelay: "100ms" }}
             >
               <div className="w-full h-48 bg-linear-to-br from-slate-700 to-slate-900 rounded-lg mb-4 flex items-center justify-center">
-                <Code className="w-16 h-16 text-blue-400" />
+                <img src={Terserahmart} alt="Project 2" className="w-full h-full object-cover" />
               </div>
-              <h4 className="text-xl font-bold text-slate-800 mb-2">E-Commerce Platform</h4>
-              <p className="text-slate-600 mb-3">Online shopping platform with product listing and UI design.</p>
+              <h4 className="text-xl font-bold text-slate-800 mb-2">E-Commerce Platform Terserahmart</h4>
+              <p className="text-slate-600 mb-3">Online shopping platform with product listing and UI design(not hosting yet).</p>
               <div className="flex items-center justify-between">
-                <span className="text-sm text-slate-500">React, Node.js</span>
+                <span className="text-sm text-slate-500">Php, JavaScript</span>
                 <ExternalLink className="w-5 h-5 text-blue-600 cursor-pointer hover:text-blue-800" />
               </div>
             </div>
@@ -91,12 +101,12 @@ const Projects = () => {
               style={{ transitionDelay: "200ms" }}
             >
               <div className="w-full h-48 bg-linear-to-br from-slate-700 to-slate-900 rounded-lg mb-4 flex items-center justify-center">
-                <Code className="w-16 h-16 text-blue-400" />
+                <img src={Coffeshop} alt="Project 3" className="w-full h-full object-cover" />
               </div>
-              <h4 className="text-xl font-bold text-slate-800 mb-2">Task Manager App</h4>
-              <p className="text-slate-600 mb-3">Task management app for productivity and workflow tracking.</p>
+              <h4 className="text-xl font-bold text-slate-800 mb-2">Coffe Shop Website</h4>
+              <p className="text-slate-600 mb-3">Coffee shop website with menu display(not hosting yet)</p>
               <div className="flex items-center justify-between">
-                <span className="text-sm text-slate-500">React, Firebase</span>
+                <span className="text-sm text-slate-500">Html, Css, JavaScript</span>
                 <ExternalLink className="w-5 h-5 text-blue-600 cursor-pointer hover:text-blue-800" />
               </div>
             </div>
@@ -122,7 +132,7 @@ const Projects = () => {
                 <img src={poster1} alt="Poster 1" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
               </div>
               <div className="p-4">
-                <p className="text-sm text-slate-500">Click to view full image</p>
+                <p className="text-sm text-slate-500">Poster hari pahlawan nasional 2025</p>
               </div>
             </div>
 
@@ -137,7 +147,7 @@ const Projects = () => {
                 <img src={Poster2} alt="Poster 2" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
               </div>
               <div className="p-4">
-                <p className="text-sm text-slate-500">Click to view full image</p>
+                <p className="text-sm text-slate-500">Poster Ucapan Selamat Wisuda</p>
               </div>
             </div>
 
@@ -152,7 +162,7 @@ const Projects = () => {
                 <img src={Poster3} alt="Poster 3" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
               </div>
               <div className="p-4">
-                <p className="text-sm text-slate-500">Click to view full image</p>
+                <p className="text-sm text-slate-500">Tipografi Cover Feed Instagram</p>
               </div>
             </div>
 
@@ -182,7 +192,7 @@ const Projects = () => {
                 <img src={Poster5} alt="Poster 5" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
               </div>
               <div className="p-4">
-                <p className="text-sm text-slate-500">Click to view full image</p>
+                <p className="text-sm text-slate-500">Tipografi Cover Feed Instagram</p>
               </div>
             </div>
 
@@ -197,9 +207,115 @@ const Projects = () => {
                 <img src={Poster6} alt="Poster 6" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
               </div>
               <div className="p-4">
-                <p className="text-sm text-slate-500">Click to view full image</p>
+                <p className="text-sm text-slate-500">Tipografi Cover Feed Instagram</p>
               </div>
             </div>
+
+            {/* Poster 7 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "900ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster7)}>
+                <img src={Poster7} alt="Poster 7" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">UI/UX CofeShop</p>
+              </div>
+            </div>
+
+              {/* Poster 8 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1000ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster8)}>
+                <img src={Poster8} alt="Poster 8" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Poster Talkshow MaroonDay 2024</p>
+              </div>
+            </div>
+
+              {/* Poster 9 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1100ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster9)}>
+                <img src={Poster9} alt="Poster 9" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Poster MLBB MaroonDay 2024</p>
+              </div>
+            </div>
+
+
+              {/* Poster 10 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1200ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster10)}>
+                <img src={Poster10} alt="Poster 10" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Banner Ramadhan Di Kampus UTDI 2025</p>
+              </div>
+            </div>
+
+              {/* Poster 11 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1300ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster11)}>
+                <img src={Poster11} alt="Poster 11" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Stiker Ramadhan Di Kampus UTDI 2025</p>
+              </div>
+            </div>
+              {/* Poster 12 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1400ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster12)}>
+                <img src={Poster12} alt="Poster 12" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Feed IG "Selamat Idul Fitri"</p>
+              </div>
+            </div>
+
+              {/* Poster 13 */}
+            <div
+              className={`bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 border border-slate-200 ${
+                visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+              }`}
+              style={{ transitionDelay: "1500ms" }}
+            >
+              <div className="w-full h-80 overflow-hidden cursor-pointer" onClick={() => setSelectedPoster(Poster13)}>
+                <img src={Poster13} alt="Poster 13" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500" />
+              </div>
+              <div className="p-4">
+                <p className="text-sm text-slate-500">Feed IG "Isra Miraj"</p>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
